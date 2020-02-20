1b610cbd652848878f94857fbef9b941907274ad
Tuyen Pham <tuh39709@temple.edu>
2020-02-20 10:15:47 -0500

    create project with 2 activities

---
+++ app/src/main/java/temple/edu/paletteactivity/CanvasActivity.java
@@ -0,0 +1,14 @@
+package temple.edu.paletteactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CanvasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_canvas);
+    }
+}
